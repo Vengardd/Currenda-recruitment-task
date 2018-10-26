12bf14c5d08d5bb1b3aa7676e004becce7f7602b
Vengardd <vengardd@users.noreply.github.com>
2018-10-26 19:28:48 +0200

    first commit

---
+++ src/main/java/com/vengard/recrutation/RecrutationApplication.java
@@ -0,0 +1,12 @@
+package com.vengard.recrutation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutationApplication.class, args);
+	}
+}
