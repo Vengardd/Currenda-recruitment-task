1e4a3ac5eab523979a2ec38ffa77d1e04d65824d
Vengardd <vengardd@users.noreply.github.com>
2018-10-27 14:32:52 +0200

    added CalculateOutputServiceTest

---
+++ src/test/java/com/vengard/recrutation/service/CalculateOutputServiceTest.java
@@ -0,0 +1,53 @@
+package com.vengard.recrutation.service;
+
+import com.vengard.recrutation.model.Currency;
+import com.vengard.recrutation.model.Input;
+import com.vengard.recrutation.model.Rate;
+import org.joda.time.LocalDate;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CalculateOutputServiceTest {
+
+    @Mock
+    private CalculateOutputService calculateOutputService;
+
+    @InjectMocks
+    private NbpResponseService nbpResponseService;
+
+    private List<Rate> list;
+
+    @Before
+    public void init() {
+        Currency currency = Currency.CurrencyBuilder.aCurrency()
+                .withCode("EUR")
+                .build();
+        LocalDate startDate = new LocalDate(2017, 11, 20);
+        LocalDate endDate = new LocalDate(2017, 11, 24);
+        Input input = new Input(currency, startDate, endDate);
+
+        list = nbpResponseService.getRateListFromNbpResponse(input);
+    }
+
+    @Test
+    public void calculateAverageBuyingRate() {
+        double result = calculateOutputService.calculateAverageBuyingRate(list);
+        assertEquals(result, 4.1815, 0);
+    }
+
+    @Test
+    public void calculateDevatationSellingRate() {
+        double result = calculateOutputService.calculateDevatationSellingRate(list);
+        assertEquals(result, 0.0101, 0);
+    }
+
+}
